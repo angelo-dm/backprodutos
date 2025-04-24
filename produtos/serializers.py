4aba967a479e467b520d42f987e899cde1d5f05b
angelo-dm <angelompr.dm@gmail.com>
2025-04-24 19:52:38 -0300

    feat: Adiciona o Serializer

--- produtos/serializers.py
+++ produtos/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from .models import Produto
 
 class ProdutoSerializer(serializers.ModelSerializer):
+    imagem = serializers.SerializerMethodField()
+
     class Meta:
         model = Produto
-        fields = '__all__'
+        fields = ['id', 'nome', 'imagem']
+
+    def get_imagem(self, obj):
+        request = self.context.get('request')
+        if obj.imagem and hasattr(obj.imagem, 'url'):
+            return request.build_absolute_uri(obj.imagem.url)
+        else:
+            return None
--- produtos/views.py
+++ produtos/views.py
@@ -5,3 +5,8 @@ from .serializers import ProdutoSerializer
 class ProdutoViewSet(viewsets.ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({"request": self.request})
+        return context
